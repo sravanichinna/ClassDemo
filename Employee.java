96164ffe82b61e35d8fb146541c69e931243a14a
sravanichinna <sravanidane413.com>
2020-11-28 16:12:04 +0530

    Added: ClassMethod logic

--- Employee.java
+++ Employee.java
@@ -1,10 +1,36 @@
 package com.Java.Class;
 
 public class Employee{
-	public static void main(String[] args){
+	int EmpId;
+	String Name;
+	double Salary;
+	static String company="Wipro";
 
-		System.out.println("Welcome to Classes,Objects & Methods");
+
+	Employee(int Id,String name,double salary)
+	{
+		EmpId=Id;
+		Name=name;
+		Salary=salary;
+
+	}
+	void display()
+	{
+		System.out.println("EmployeeId: "+EmpId+ "Name: "+Name+ "Salary: "+Salary+ "Company: "+company);
+
+
 	}
 
-}
 
+}
+ class details {
+	public static void main(String[] args) {
+		Employee E1=new Employee(123,"Sravani",62000.00);
+		Employee E2=new Employee(124,"Kalyani",30000.00);
+		E1.display();
+		E2.display();
+
+	}
+
+
+}
